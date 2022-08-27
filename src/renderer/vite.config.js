f8821295da078bdb531887df2d342e0a6ad97d26
Konsey <vgbukalders@gmail.com>
2022-08-27 15:50:46 +0300

    delete unused files

--- src/renderer/vite.config.js
+++ src/renderer/vite.config.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 
-import {chrome} from '../../.electron-vendors.cache.json';
-import {join} from 'path';
+import { chrome } from '../../.electron-vendors.cache.json';
+import { join } from 'path';
 import vue from '@vitejs/plugin-vue';
-import {renderer} from 'unplugin-auto-expose';
+import { renderer } from 'unplugin-auto-expose';
 
 const PACKAGE_ROOT = __dirname;
 
@@ -38,6 +38,9 @@ const config = {
   },
   test: {
     environment: 'happy-dom',
+  },
+  css: {
+    devSourcemap: true,
   },
   plugins: [
     vue(),
